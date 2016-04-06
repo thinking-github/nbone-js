5f0a139b42cb62d96ac8e8db8af95e935bdd597b
ChenYiCheng0 <chenyicheng00@gmail.com>
2016-04-06 09:50:45 +0800

    init nbone-js

---
+++ thinkloader.js
@@ -0,0 +1,241 @@
+﻿/**
+ * easyloader - jQuery ThinkUI
+ * 
+ */
+(function(){
+	var modules = {
+		
+		linkbutton:{
+			js:'jquery.linkbutton.js',
+			css:'linkbutton.css'
+		},
+		parser:{
+			js:'jquery.parser.js'
+		}
+	};
+	
+	var locales = {
+		'af':'easyui-lang-af.js',
+		'ar':'easyui-lang-ar.js',
+		'bg':'easyui-lang-bg.js',
+		'ca':'easyui-lang-ca.js',
+		'cs':'easyui-lang-cs.js',
+		'cz':'easyui-lang-cz.js',
+		'da':'easyui-lang-da.js',
+		'de':'easyui-lang-de.js',
+		'el':'easyui-lang-el.js',
+		'en':'easyui-lang-en.js',
+		'es':'easyui-lang-es.js',
+		'fr':'easyui-lang-fr.js',
+		'it':'easyui-lang-it.js',
+		'jp':'easyui-lang-jp.js',
+		'nl':'easyui-lang-nl.js',
+		'pl':'easyui-lang-pl.js',
+		'pt_BR':'easyui-lang-pt_BR.js',
+		'ru':'easyui-lang-ru.js',
+		'sv_SE':'easyui-lang-sv_SE.js',
+		'tr':'easyui-lang-tr.js',
+		'zh_CN':'easyui-lang-zh_CN.js',
+		'zh_TW':'easyui-lang-zh_TW.js'
+	};
+	
+	var queues = {};
+	
+	function loadJs(url, callback){
+		var done = false;
+		var script = document.createElement('script');
+		script.type = 'text/javascript';
+		script.language = 'javascript';
+		script.src = url;
+		script.onload = script.onreadystatechange = function(){
+			if (!done && (!script.readyState || script.readyState == 'loaded' || script.readyState == 'complete')){
+				done = true;
+				script.onload = script.onreadystatechange = null;
+				if (callback){
+					callback.call(script);
+				}
+			}
+		}
+		document.getElementsByTagName("head")[0].appendChild(script);
+	}
+	
+	function runJs(url, callback){
+		loadJs(url, function(){
+			document.getElementsByTagName("head")[0].removeChild(this);
+			if (callback){
+				callback();
+			}
+		});
+	}
+	
+	function loadCss(url, callback){
+		var link = document.createElement('link');
+		link.rel = 'stylesheet';
+		link.type = 'text/css';
+		link.media = 'screen';
+		link.href = url;
+		document.getElementsByTagName('head')[0].appendChild(link);
+		if (callback){
+			callback.call(link);
+		}
+	}
+	
+	function loadSingle(name, callback){
+		queues[name] = 'loading';
+		
+		var module = modules[name];
+		var jsStatus = 'loading';
+		var cssStatus = (easyloader.css && module['css']) ? 'loading' : 'loaded';
+		
+		if (easyloader.css && module['css']){
+			if (/^http/i.test(module['css'])){
+				var url = module['css'];
+			} else {
+				var url = easyloader.base + 'themes/' + easyloader.theme + '/' + module['css'];
+			}
+			loadCss(url, function(){
+				cssStatus = 'loaded';
+				if (jsStatus == 'loaded' && cssStatus == 'loaded'){
+					finish();
+				}
+			});
+		}
+		
+		if (/^http/i.test(module['js'])){
+			var url = module['js'];
+		} else {
+			var url = easyloader.base + 'plugins/' + module['js'];
+		}
+		loadJs(url, function(){
+			jsStatus = 'loaded';
+			if (jsStatus == 'loaded' && cssStatus == 'loaded'){
+				finish();
+			}
+		});
+		
+		function finish(){
+			queues[name] = 'loaded';
+			easyloader.onProgress(name);
+			if (callback){
+				callback();
+			}
+		}
+	}
+	
+	function loadModule(name, callback){
+		var mm = [];
+		var doLoad = false;
+		
+		if (typeof name == 'string'){
+			add(name);
+		} else {
+			for(var i=0; i<name.length; i++){
+				add(name[i]);
+			}
+		}
+		
+		function add(name){
+			if (!modules[name]) return;
+			var d = modules[name]['dependencies'];
+			if (d){
+				for(var i=0; i<d.length; i++){
+					add(d[i]);
+				}
+			}
+			mm.push(name);
+		}
+		
+		function finish(){
+			if (callback){
+				callback();
+			}
+			easyloader.onLoad(name);
+		}
+		
+		var time = 0;
+		function loadMm(){
+			if (mm.length){
+				var m = mm[0];	// the first module
+				if (!queues[m]){
+					doLoad = true;
+					loadSingle(m, function(){
+						mm.shift();
+						loadMm();
+					});
+				} else if (queues[m] == 'loaded'){
+					mm.shift();
+					loadMm();
+				} else {
+					if (time < easyloader.timeout){
+						time += 10;
+						setTimeout(arguments.callee, 10);
+					}
+				}
+			} else {
+				if (easyloader.locale && doLoad == true && locales[easyloader.locale]){
+					var url = easyloader.base + 'locale/' + locales[easyloader.locale];
+					runJs(url, function(){
+						finish();
+					});
+				} else {
+					finish();
+				}
+			}
+		}
+		
+		loadMm();
+	}
+	
+	easyloader = {
+		modules:modules,
+		locales:locales,
+		
+		base:'.',
+		theme:'ruby',
+		css:true,
+		locale:null,
+		timeout:2000,
+	
+		load: function(name, callback){
+			if (/\.css$/i.test(name)){
+				if (/^http/i.test(name)){
+					loadCss(name, callback);
+				} else {
+					loadCss(easyloader.base + name, callback);
+				}
+			} else if (/\.js$/i.test(name)){
+				if (/^http/i.test(name)){
+					loadJs(name, callback);
+				} else {
+					loadJs(easyloader.base + name, callback);
+				}
+			} else {
+				loadModule(name, callback);
+			}
+		},
+		
+		onProgress: function(name){},
+		onLoad: function(name){}
+	};
+
+	var scripts = document.getElementsByTagName('script');
+	for(var i=0; i<scripts.length; i++){
+		var src = scripts[i].src;
+		if (!src) continue;
+		var m = src.match(/easyloader\.js(\W|$)/i);
+		if (m){
+			easyloader.base = src.substring(0, m.index);
+		}
+	}
+
+	window.using = easyloader.load;
+	
+	if (window.jQuery){
+		jQuery(function(){
+			easyloader.load('parser', function(){
+				jQuery.parser.parse();
+			});
+		});
+	}
+	
+})();
